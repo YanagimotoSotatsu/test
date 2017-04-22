130a4e04acc4184ea2e288a9ece38370b4646f11
柳本宗達 <juventus.y.1104@gmail.com>
2017-04-22 16:29:09 +0900

    確認テスト

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -9,10 +9,51 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var label: UILabel!
+    var i: Int = 0
+    
+    let wordArray : [String] = ["iPhone", "iPad", "Apple Watch"]
+
+    
+
+    @IBAction func mae(){
+        if i >= 1{
+        i = i - 1
+            label.text = wordArray[i]
+        }else if i == 0{
+            i = 2
+            label.text = wordArray[i]
+
+        }
+    }
+    @IBAction func tugi(){
+        if i <= 1{
+            i = i + 1
+            label.text = wordArray[i]
+        }else if i == 2{
+            i = 0
+            label.text = wordArray[i]
+
+        }
+        
+    }
+    @IBAction func ichi(){
+        label.text = wordArray[0]
+        
+    }
+    @IBAction func ni(){
+        label.text = wordArray[1]
+    }
+    @IBAction func sann(){
+        label.text = wordArray[2]
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        label.text = wordArray[i]
+
     }
 
     override func didReceiveMemoryWarning() {
